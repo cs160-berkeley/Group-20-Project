853826e0cb159e2fc51b0c10a6e3cca9f8327655
Patricia Xiao <patricia.xiao@berkeley.edu>
2016-11-07 22:34:10 -0800

    welcome screen added
    I added the welcome screen, according to the mockup.

--- port-companion/global_settings.js
+++ port-companion/global_settings.js
@@ -1,4 +1,18 @@
-
+// temporary screen
+export var TMP_SCREEN;
+// parameters
+export var welcome_img_padding = 45;
+export var welcome_img_size = 90;
+export var welcome_button_height = 36;
+// images
 var img_dir = "./assets/";
 export var img_SwitchBGD = img_dir + "switchBackground.png";
 export var img_SwitchFGD = img_dir + "switchForeground.png";
+export var img_logo = img_dir + "Group.png";
+// colors
+export var lightGray = "#F2F2F2";
+export var darkGray = "#202020";
+// skins
+export var whiteSkin = new Skin({ fill: "white" });
+// text stylesexport var titleText = new Style({ font: "bold 18px", color: darkGray });
+export var buttonText = new Style({ font: "bold 18px", color: "#DDDDDD" });
--- port-companion/main.js
+++ port-companion/main.js
@@ -1,215 +1,33 @@
-/* main page: home */
-
+import {
+	TMP_SCREEN,	img_logo,	whiteSkin,
+	titleText,
+	welcome_img_padding,
+	welcome_img_size,
+	welcome_button_height,
+	buttonText,} from "global_settings";import { 	Button,    ButtonBehavior,    RadioGroup,     RadioGroupBehavior} from 'lib/buttons';
 
 import {
-	img_SwitchBGD,
-	img_SwitchFGD,
-} from "global_settings";
+	HomeScreen
+} from "home";
 
-var deviceURL = "";
-let footerSkin = new Skin({ fill:"white" });
-let toggleSwitchBackgroundTexture = new Texture(img_SwitchBGD);
-let toggleSwitchForegroundTexture = new Texture(img_SwitchFGD);
+var padding = welcome_img_padding;
+var size = welcome_img_size;
 
-let labelStyle = new Style({ font:"20px Verdana", color:"black" });
-	
-/* BEHAVIORS */
-
-class ToggleSwitchBehavior extends Behavior {
-	constrain(port, offset) {
-		if (offset < 0)
-			offset = 0;
-		else if (offset > this.size)
-			offset = this.size;
-		return offset;
-	}
-	onCreate(port, data) {
-		this.data = data;
-		this.onOff = this.data.onOff;
-		this.fd_onOff = this.data.fd_onOff;
-		this.switchForeground = toggleSwitchForegroundTexture;
-		this.switchBackground = toggleSwitchBackgroundTexture;
-		
-		this.size = this.switchBackground.width - this.switchForeground.width;
-		this.offset = ("off" == this.onOff ? this.size : 0);
-		this.touched = false;
-		this.capturing = false;
-		this.touchMovedOffset = 15;
-	}
-	onDraw(port, x, y, width, height) {
-		port.pushClip();
-		port.intersectClip(x + 6, y + 6, width - 12, height - 12);
-		port.drawImage(this.switchBackground, x - this.offset, y, width + this.size, height, 
-			0, this.touched ? 40 : 0, width + this.size, height);
-		port.popClip();
-		//port.drawImage(this.switchForeground, x, y, width, height);
-	}
-	onFinished(port) {
-		this.touched = false;
-		this.capturing = false;
-		
-		if (this.offset == 0)
-			this.onOff = "on";
-		else
-			this.onOff = "off";
-			
-		this.onSwitchChanged(port, this.onOff);
-	}
-	onSwitchChanged(port, onOff) {
-		debugger
-	}
-	onTimeChanged(port) {
-		let fraction = port.fraction;
-		this.offset = this.anchor + Math.round(this.delta * fraction);
-		port.invalidate();
-	}
-	onTouchBegan(port, id, x, y, ticks) {
-		if (port.running) {
-			port.stop();
-			port.time = port.duration;
-		}
-		this.anchor = x;
-		this.delta = this.offset + x;
-		this.touched = true;
-		port.invalidate();
-	}
-	onTouchCancelled(port, id, x, y, ticks) {
-		this.touched = false;
-	}
-	onTouchEnded(port, id, x, y, ticks) {
-		let offset = this.offset;
-		let size = this.size;
-		let delta = size >> 1;
-		this.anchor = offset;
-		if (this.capturing) {
-			if (offset < delta)
-				delta = 0 - offset;
-			else 
-				delta = size - offset;
-		}
-		else {
-			if (offset == 0)
-				delta = size;
-			else
-				delta = 0 - size;
-		}
-		if (delta) {
-			this.delta = delta;
-			port.duration = 100 * Math.abs(delta) / size;
-			port.time = 0;
-			port.start();
-		}
-		else
-			this.onFinished(port);
-		port.invalidate();
-	}
-	onTouchMoved(port, id, x, y, ticks) {
-		if (this.capturing)
-			this.offset = this.constrain(port, this.delta - x);
-		else if (Math.abs(x - this.anchor) >= this.touchMovedOffset) {
-			port.captureTouch(id, x, y, ticks);
-			this.capturing = true;
-			this.offset = this.constrain(port, this.delta - x);
-		}
-		port.invalidate();
-	}
-}
-
-/* LAYOUTS */
-
-let test = Container.template($ => ( {
-			top:100, right:30, height:0, skin:footerSkin,
-			contents: [
-				Port($, {
-					width:toggleSwitchForegroundTexture.width, height:toggleSwitchForegroundTexture.height, active:true,
-					Behavior: class extends ToggleSwitchBehavior {
-						onSwitchChanged(port, onOff) {
-							if (deviceURL != "") new Message(deviceURL + "getCount").invoke(Message.JSON).then(json => { this.data.onOff = json.count });
-							this.data.onOff = onOff;
-							// port.container.previous.invalidate();
-						}
-					}
-				})
+let enterButtonTemplate = Button.template($ => ({    top: 0, width: 200, right: 0, height: welcome_button_height,    contents: [        Label($, {left: 0, right: 0, height: welcome_button_height, string: $.textForLabel, style: buttonText})    ],    Behavior: class extends ButtonBehavior {        onTap(button){      
+        	application.remove(TMP_SCREEN);
+        	TMP_SCREEN = new HomeScreen({ onOff:"on" });
+        	application.add(TMP_SCREEN);        }
+            }}));var WelcomePageTemplate = Container.template($ => ({     left: 0, right: 0, top: 0, bottom: 0,    skin: $.skin,    contents: [
+    	new Column({ 
+    		top: 0,
+    		contents: [		    	new Picture({					url: $.img_url,					top: padding, left: padding, right: padding, width: size, height: size,				}),
+				new Label({
+					string: $.title,
+					style: titleText
+				}),
+				new enterButtonTemplate({textForLabel: "ENTER YOUR HOME"}),
+				
 			]
-		}));
-
-let MainScreen = Container.template($ => ({
-	left:0, right:0, top:0, bottom:0,
-	contents: [
-		Port($, {
-			left:0, right:0, top:0, bottom:0, style:labelStyle,
-			Behavior: class extends Behavior {
-				onCreate(port, data) {
-					this.data = data;
-				}
-				onDraw(port, x, y, width, height) {
-					port.fillColor( "white", x, y, width, height );
-					port.drawLabel( "off" == this.data.onOff ? "Night Light: Off" : "Night Light: On" , -80, -140, width, height );
-					port.drawLabel( "off" == this.data.fd_onOff ? "Front Door: Off" : "Front Door: On" , -80, -90, width, height );
-					port.drawLabel( "off" == this.data.ov_onOff ? "Oven: Off" : "Oven: On" , -80, -40, width, height );
-				}
-				onSwitchChanged(port, onOff) {
-					port.invalidate();
-				}
-			}
-		}),
-		// new test({$}),
-		new test({}),
-		/*
-		Container($, {
-			top:100, right:30, height:0, skin:footerSkin,
-			contents: [
-				Port($, {
-					width:toggleSwitchForegroundTexture.width, height:toggleSwitchForegroundTexture.height, active:true,
-					Behavior: class extends ToggleSwitchBehavior {
-						onSwitchChanged(port, onOff) {
-							if (deviceURL != "") new Message(deviceURL + "getCount").invoke(Message.JSON).then(json => { this.data.onOff = json.count });
-							this.data.onOff = onOff;
-							port.container.previous.invalidate();
-						}
-					}
-				})
-			]
-		}),
-		*/
-		Container($, {
-			top:150, right:30, height:0, skin:footerSkin,
-			contents: [
-				Port($, {
-					width:toggleSwitchForegroundTexture.width, height:toggleSwitchForegroundTexture.height, active:true,
-					Behavior: class extends ToggleSwitchBehavior {
-						onSwitchChanged(port, fd_onOff) {
-							if (deviceURL != "") new Message(deviceURL + "getFrontDoor").invoke(Message.JSON).then(json => { this.data.fd_onOff = json.fd_count });
-							this.data.fd_onOff = fd_onOff;
-							// port.container.previous.invalidate();
-						}
-					}
-				})
-			]
-		}),
-		Container($, {
-			top:200, right:30, height:0, skin:footerSkin,
-			contents: [
-				Port($, {
-					width:toggleSwitchForegroundTexture.width, height:toggleSwitchForegroundTexture.height, active:true,
-					Behavior: class extends ToggleSwitchBehavior {
-						onSwitchChanged(port, ov_onOff) {
-							if (deviceURL != "") new Message(deviceURL + "getOven").invoke(Message.JSON).then(json => { this.data.ov_onOff = json.ov_count });
-							this.data.ov_onOff = ov_onOff;
-							// port.container.previous.invalidate();
-						}
-					}
-				})
-			]
-		})
-	]
-}));
-
-Handler.bind("/discover", Behavior({    onInvoke: function(handler, message){        deviceURL = JSON.parse(message.requestText).url;    }}));Handler.bind("/forget", Behavior({    onInvoke: function(handler, message){        deviceURL = "";    }}));
-
-var ApplicationBehavior = Behavior.template({    onDisplayed: function(application) {        application.discover("portdevice.project.kinoma.marvell.com");        application.add(new MainScreen({ onOff:"on" }));    },    onQuit: function(application) {        application.forget("portdevice.project.kinoma.marvell.com");    },});
-
-/* APPLICATION */
-
-application.behavior = new ApplicationBehavior();
-
+		}),    ]}));
+TMP_SCREEN = new WelcomePageTemplate({ skin: whiteSkin, img_url: img_logo, title: "HoM"});
+application.add(TMP_SCREEN);
