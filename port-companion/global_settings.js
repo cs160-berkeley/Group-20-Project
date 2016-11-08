ee347a4190c6891bd77f05bcc019ec5214eebdbe
Patricia Xiao <patricia.xiao@berkeley.edu>
2016-11-07 20:00:06 -0800

    tiny change
    bug discovered & structure changed

---
+++ port-companion/global_settings.js
@@ -0,0 +1,4 @@
+
+var img_dir = "./assets/";
+export var img_SwitchBGD = img_dir + "switchBackground.png";
+export var img_SwitchFGD = img_dir + "switchForeground.png";
--- port-companion/main.js
+++ port-companion/main.js
@@ -1,26 +1,13 @@
-//@program
-/*
-  Copyright 2011-2016 Marvell Semiconductor, Inc.
-  
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-  
-      http://www.apache.org/licenses/LICENSE-2.0
-      
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
 
-/* ASSETS */
-trace("Hello");
+import {
+	img_SwitchBGD,
+	img_SwitchFGD,
+} from "global_settings";
+
 var deviceURL = "";
 let footerSkin = new Skin({ fill:"white" });
-let toggleSwitchBackgroundTexture = new Texture("switchBackground.png");
-let toggleSwitchForegroundTexture = new Texture("switchForeground.png");
+let toggleSwitchBackgroundTexture = new Texture(img_SwitchBGD);
+let toggleSwitchForegroundTexture = new Texture(img_SwitchFGD);
 
 let labelStyle = new Style({ font:"20px Verdana", color:"black" });
 	
@@ -187,7 +174,7 @@ let MainScreen = Container.template($ => ({
 						onSwitchChanged(port, ov_onOff) {
 							if (deviceURL != "") new Message(deviceURL + "getOven").invoke(Message.JSON).then(json => { this.data.ov_onOff = json.ov_count });
 							this.data.ov_onOff = ov_onOff;
-							port.container.previous.invalidate();
+							port.container.previous.invalidate(); // have bug when calling it, please fix it @Mo
 						}
 					}
 				})
