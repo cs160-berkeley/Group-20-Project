bc4d6ee27fc7b228cdacb40b2c7a993ae9428750
Jackie <JackieG.017@gmail.com>
2016-11-09 15:54:52 -0800

    implemented timing feature

--- port-companion/main.js
+++ port-companion/main.js
@@ -8,7 +8,9 @@
  */
 
 import {
-	TMP_SCREEN,	img_logo,	whiteSkin,
+	TMP_SCREEN,
+	img_logo,
+	whiteSkin,
 	titleText,
 	welcome_img_padding,
 	welcome_img_size,
@@ -17,7 +19,15 @@ import {
 	DeviceSimulator,
 	deviceURL,
 	load_data,
-	DATA,} from "global_settings";import { 	Button,    ButtonBehavior,    RadioGroup,     RadioGroupBehavior} from 'lib/buttons';
+	DATA,
+} from "global_settings";
+
+import { 
+	Button,
+    ButtonBehavior,
+    RadioGroup, 
+    RadioGroupBehavior
+} from 'lib/buttons';
 
 import {
 	HomeContentTemplate,
@@ -34,19 +44,36 @@ var size = welcome_img_size;
 DATA = load_data();
 
 // the button to enter the home page
-let enterButtonTemplate = Button.template($ => ({    top: 0, width: 200, right: 0, height: welcome_button_height,    contents: [        Label($, {left: 0, right: 0, height: welcome_button_height, string: $.textForLabel, style: buttonText})    ],    Behavior: class extends ButtonBehavior {        onTap(button){      
+let enterButtonTemplate = Button.template($ => ({
+    top: 0, width: 200, right: 0, height: welcome_button_height,
+    contents: [
+        Label($, {left: 0, right: 0, height: welcome_button_height, string: $.textForLabel, style: buttonText})
+    ],
+    Behavior: class extends ButtonBehavior {
+        onTap(button){      
         	application.remove(TMP_SCREEN);
         	HomeContent = HomeContentTemplate({});
         	LoadHomeContent(HomeContent);
         	HomeScreen = new HomeScreenTemplate({ HomeContent });
         	TMP_SCREEN = HomeScreen;
-        	application.add(TMP_SCREEN);        }
-            }}));
+        	application.add(TMP_SCREEN);
+        }
+        
+    }
+}));
 
-// the welcome page main contentvar WelcomePageTemplate = Container.template($ => ({     left: 0, right: 0, top: 0, bottom: 0,    skin: $.skin,    contents: [
+// the welcome page main content
+var WelcomePageTemplate = Container.template($ => ({ 
+    left: 0, right: 0, top: 0, bottom: 0,
+    skin: $.skin,
+    contents: [
     	new Column({ 
     		top: padding, bottom: padding,
-    		contents: [		    	new Picture({					url: $.img_url,					top: padding, left: padding, right: padding, width: size, height: size,				}),
+    		contents: [
+		    	new Picture({
+					url: $.img_url,
+					top: padding, left: padding, right: padding, width: size, height: size,
+				}),
 				new Label({
 					string: $.title,
 					style: titleText
@@ -54,15 +81,34 @@ let enterButtonTemplate = Button.template($ => ({    top: 0, width: 200, right:
 				new enterButtonTemplate({textForLabel: "ENTER YOUR HOME"}),
 				
 			]
-		}),    ]}));
+		}),
+    ]
+}));
 
 /* behavior of connecting the devices */
 // connected by port
-Handler.bind("/discover", Behavior({    onInvoke: function(handler, message){
-    	trace("found the device.\n");        deviceURL = JSON.parse(message.requestText).url;    }}));Handler.bind("/forget", Behavior({    onInvoke: function(handler, message){
-    	trace("forget the device.\n");        deviceURL = "";    }}));
+Handler.bind("/discover", Behavior({
+    onInvoke: function(handler, message){
+    	trace("found the device.\n");
+        deviceURL = JSON.parse(message.requestText).url;
+    }
+}));
+Handler.bind("/forget", Behavior({
+    onInvoke: function(handler, message){
+    	trace("forget the device.\n");
+        deviceURL = "";
+    }
+}));
 // will be used to communicate between device and the companion application
-var ApplicationBehavior = Behavior.template({    onDisplayed: function(application) {	        application.discover(DeviceSimulator);        // application.add(new HomeScreen({ onOff:"on" }));    },    onQuit: function(application) {        application.forget(DeviceSimulator);    },});
+var ApplicationBehavior = Behavior.template({
+    onDisplayed: function(application) {	
+        application.discover(DeviceSimulator);
+        // application.add(new HomeScreen({ onOff:"on" }));
+    },
+    onQuit: function(application) {
+        application.forget(DeviceSimulator);
+    },
+});
 
 // initialize the welcome screen
 TMP_SCREEN = new WelcomePageTemplate({ skin: whiteSkin, img_url: img_logo, title: "HoM"});
